a8a0d9df36c82e3ff6daf3c7fc1f845407b468c9
edustef <eduardstefan290@gmail.com>
2019-09-19 00:56:42 +0200

    separated youtube api calls into different js file

---
+++ public/js/yt.js
@@ -0,0 +1,36 @@
+fetch = require('node-fetch');
+
+module.exports.getVideoData = async function(ytId) {
+  const key = process.env.API_KEY;
+  const url =
+    'https://www.googleapis.com/youtube/v3/videos?id=' +
+    ytId +
+    '&key=' +
+    key +
+    '&part=snippet';
+  const response = await fetch(url);
+  const commits = await response.json();
+  return await commits.items[0].snippet;
+};
+
+module.exports.findId = function(ytURL) {
+  const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  const match = ytURL.match(regExp);
+  if (match && match[2].length == 11) {
+    return match[2];
+  } else {
+    console.log('There was an error!');
+  }
+};
+
+module.exports.getComments = async function(ytId) {
+  const key = process.env.API_KEY;
+  const url =
+    'https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&maxResults=10&order=relevance&textFormat=plainText&videoId=' +
+    ytId +
+    '&key=' +
+    key;
+  const response = await fetch(url);
+  const commits = await response.json();
+  return await commits;
+};
