9d1bc80da02a2eae8cc931d62fde2a9afd740002
edustef <eduardstefan290@gmail.com>
2019-09-21 00:03:01 +0200

    removed likes because it doesnt fetch any

--- models/comment.js
+++ models/comment.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const commentSchema = new mongoose.Schema({
   authorDisplayName: String,
   authorProfileImageUrl: String,
-  text: String,
-  likes: String
+  text: String
 });
 
 module.exports = mongoose.model('Comment', commentSchema);
