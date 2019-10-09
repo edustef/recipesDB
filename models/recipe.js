d8c16c3ad362a03888e90c6ecdf2a2ef82278beb
Eduard <eduardstefan290@gmail.com>
2019-10-09 23:01:53 +0200

    added user reference and removed mongoose require from seeds

--- models/recipe.js
+++ models/recipe.js
@@ -6,6 +6,13 @@ const recipeSchema = new mongoose.Schema({
   image: { type: String, default: 'https://images.unsplash.com/photo-1490818387583-1baba5e638af?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60' },
   article: String,
   ytId: String,
+  user: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    username: String,
+  },
   comments: [
     {
       type: mongoose.Schema.Types.ObjectId,
--- public/js/seeds.js
+++ public/js/seeds.js
@@ -1,11 +1,10 @@
-const mongoose = require('mongoose'),
-  yt = require('./yt'),
+const yt = require('./yt'),
   Recipe = require('../../models/recipe'),
   Comment = require('../../models/comment');
 
 const seeds = async body => {
   if (body.ytURL) {
-    let id = yt.findId(body.ytURL);
+    let id = yt.findId(body.ytURL); 
     videoData = await yt.getVideoData(id);
     comments = (await yt.getComments(id)).items;
 
